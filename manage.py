fbaab5ed5abe151e9cd3308ee1705bd2c5450e36
Mathias Bockwoldt <mbo049@post.uit.no>
2016-06-27 16:15:41 +0200

    Preventing double migration (not very safe, but better than nothing)

--- manage.py
+++ manage.py
@@ -7,4 +7,9 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    if sys.argv[-1] == 'migrate':
+		if os.path.isfile('db.sqlite3'):
+			print('You tried to migrate although db.sqlite3 already exists.\nPlease delete db.sqlite3 before you migrate again.')
+			sys.exit()
+
     execute_from_command_line(sys.argv)
