10e9f73514648b4b1d7401ed2f16e7f60417bdbd
Mathias Bockwoldt <mbo049@post.uit.no>
2016-08-04 15:17:31 +0200

    Batch Mode works now with Example Files (minor bug resolved)

--- sbmlmod/views.py
+++ sbmlmod/views.py
@@ -211,7 +211,7 @@ def __setup_session(request, form):
         submitted_input['kinetic_law_data_file'] = request.FILES.get('kinetic_law_data_file', StringIO.StringIO()).read()
         submitted_input['species_data_file'] = request.FILES.get('species_data_file', StringIO.StringIO()).read()
     else:
-        submitted_input['batch_mode'] = False
+        submitted_input['batch_mode'] = form.cleaned_data['batch_mode']
         submitted_input['kl_column'] = 2
         submitted_input['s_column'] = 2
         sbml_file, mapping_file, kinetic_law_data_file, species_data_file = __read_example_files()
