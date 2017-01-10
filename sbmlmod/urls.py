cd937f0ee5515381c4f2bda446c2376d31ec81bd
Mathias Bockwoldt <mathias.bockwoldt@uit.no>
2017-01-10 14:20:11 +0100

    Graphical overviews are now created after CopasiWS runs

--- sbmlmod/urls.py
+++ sbmlmod/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^help/$', views.help, name='help'),
     url(r'^results/$', views.results, name='results'),
     url(r'^results/get_models_zipped/$', views.get_models_zipped, name='get_models_zipped'),
+    url(r'^results/image/$', views.get_image, name='get_image'),
     url(r'^results/get_simulations_zipped/$', views.get_simulations_zipped, name='get_simulations_zipped'),
-    url(r'^results/(?P<name>\w+)/$', views.get_model, name='get_model'),
-    url(r'^results/simulations/(?P<name>\w+)/$', views.get_simulation, name='get_simulation')
+    url(r'^results/(?P<name>[a-zA-Z0-9_\.-]+)/$', views.get_model, name='get_model'),
+    url(r'^results/simulations/(?P<name>[a-zA-Z0-9_\.-]+)/$', views.get_simulation, name='get_simulation')
 ]
